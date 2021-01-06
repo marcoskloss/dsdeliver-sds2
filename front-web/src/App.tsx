350bb182c1fbf990a6edfb4bc5c7ab9517e7fe79
marcoskloss <marcoskloss2001@gmail.com>
2021-01-06 20:48:08 -0300

    Add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <Navbar />
+    <> 
+      <Navbar />
+      <Home />
+    </>
   );
 }
 
