49289a6ee8b5f971d0e652a36ff80e9cb20d1edf
marcoskloss <marcoskloss2001@gmail.com>
2021-01-08 22:16:11 -0300

    Finish front-web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { OrderPayload } from "./Orders/types";
 
 // const API_URL = 'http://localhost:8080';
 const API_URL = 'https://marcoskloss-sds2.herokuapp.com';
@@ -10,4 +11,8 @@ export function fetchProducts() {
 
 export function fetchLocalMapBox(local: string) {
   return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrder(payload: OrderPayload) {
+  return axios.post(`${API_URL}/orders`, payload);
 }
